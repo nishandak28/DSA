e8c9a0ef8659dcf44361ba8ae3c1e5dce2a2dc90
NISHAND AK <nishandak4141@gmail.com>
2026-08-11 11:40:38 +0530

    Time: 7 ms (73.63%) | Memory: 117 MB (62.80%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -17,22 +17,49 @@ class Solution {
     //     return Math.min(onestep, Math.min(twostep, threestep));
     // }
 
-    public int climbStairs(int n, int[] costs) {
+
+            // DP + BACKTRACKING
+
+    // public int climbStairs(int n, int[] costs) {
+    //     int dp[] = new int[n+1];
+    //     Arrays.fill(dp, -1);
+    //     return solve(0, n , costs, dp);
+    // }
+
+    // public int solve(int i, int n, int[] costs, int[] dp) {
+    //     if(i > n) return Integer.MAX_VALUE;
+    //     if(i == n) return 0;
+
+    //     if(dp[i] != -1) return dp[i];
+
+    //     int onestep = (i+1 <= n) ? (costs[i] + 1)+ solve(i + 1,n,costs, dp): Integer.MAX_VALUE;
+    //     int twostep = (i+2 <= n) ? (costs[i+1] + 4) + solve(i + 2,n,costs, dp): Integer.MAX_VALUE;
+    //     int threestep = (i+3 <= n) ? (costs[i+2] + 9) + solve(i + 3,n,costs, dp): Integer.MAX_VALUE;
+
+    //     return dp[i] = Math.min(onestep, Math.min(twostep, threestep));
+    // }
+
+
+     public int climbStairs(int n, int[] costs) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(0, n , costs, dp);
+        dp[0] = 0;
+
+        for(int j = 1; j < dp.length; j++) {
+            dp[j] = Integer.MAX_VALUE;
+            
+            if(j-1 >= 0) {
+                dp[j] = Math.min(dp[j] , dp[j-1] + costs[j-1] + 1);
+            } 
+            if(j-2 >= 0) {
+                dp[j] = Math.min(dp[j] , dp[j-2] + costs[j-1] + 4);
+            }
+            if(j-3 >= 0) {
+                dp[j] = Math.min(dp[j] , dp[j-3] + costs[j-1] + 9);
+            }
+        }
+
+        return dp[n];
     }
 
-    public int solve(int i, int n, int[] costs, int[] dp) {
-        if(i > n) return Integer.MAX_VALUE;
-        if(i == n) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int onestep = (i+1 <= n) ? (costs[i] + 1)+ solve(i + 1,n,costs, dp): Integer.MAX_VALUE;
-        int twostep = (i+2 <= n) ? (costs[i+1] + 4) + solve(i + 2,n,costs, dp): Integer.MAX_VALUE;
-        int threestep = (i+3 <= n) ? (costs[i+2] + 9) + solve(i + 3,n,costs, dp): Integer.MAX_VALUE;
-
-        return dp[i] = Math.min(onestep, Math.min(twostep, threestep));
-    }
+    
 }
