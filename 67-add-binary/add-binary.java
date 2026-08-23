49064b143c10666b31057a4fba9a1a1d249078e4
NISHAND AK <nishandak4141@gmail.com>
2026-08-23 18:39:19 +0530

    Time: 2 ms (46.44%) | Memory: 43.6 MB (53.20%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,12 +1,42 @@
 import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        
-        BigInteger bs1 = new BigInteger(a, 2);
-        BigInteger bs2 = new BigInteger(b, 2);
 
-        BigInteger sum = bs1.add(bs2);
+        StringBuffer sb = new StringBuffer("");
+        int i = a.length()-1, j = b.length()-1, carry = 0;
 
-        return sum.toString(2);
+        while(i >= 0 || j >= 0 || carry > 0) {
+
+            int sum = carry; // instead of sum+carry assing this;
+
+            if(i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if( j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            sb.append(sum %2);
+            carry = sum/2;
+        }
+
+        return sb.reverse().toString();
     }
 }
+
+
+
+// class Solution {
+//     public String addBinary(String a, String b) {
+        
+//         BigInteger bs1 = new BigInteger(a, 2);
+//         BigInteger bs2 = new BigInteger(b, 2);
+
+//         BigInteger sum = bs1.add(bs2);
+
+//         return sum.toString(2);
+//     }
+// }
